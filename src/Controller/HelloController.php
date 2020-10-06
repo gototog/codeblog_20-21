57262b4d8c333b209f848c6f69545a381c4686c1
Renaud Bredy <renaud.bredy@kibatic.com>
2020-10-06 16:09:31 +0200

    hello routes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,5 +8,38 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
+    /**
+     * @Route("/hello/world", name="hello_world")
+     */
+    public function hello()
+    {
+        return new Response(
+            '<html><body>Hello WORLD!</body></html>'
+        );
+    }
+
+    /**
+     * @Route("/hello/{number}", name="hello_number", requirements={"number"="\d+"})
+     */
+    public function helloNumber($number)
+    {
+        $worlds = '';
+        for ($i = 1; $i <= $number; $i++) {
+            $worlds = $worlds . ' world';
+        }
+        return new Response(
+            '<html><body>Hello ' . $worlds . '</body></html>'
+        );
+    }
+
+    /**
+     * @Route("/hello/{firstname}", name="hello_firstname")
+     */
+    public function helloFirstname($firstname)
+    {
+        return new Response(
+            '<html><body>Hello ' . $firstname . '</body></html>'
+        );
+    }
 
 }
